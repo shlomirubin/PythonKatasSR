91845937d506138ed4036df497c5651557b1367d
alonit <alon.itach@mail.huji.ac.il>
2024-09-12 21:49:33 +0300

    initial commit

---
+++ katas/__init__.py
@@ -0,0 +1,28 @@
+katas = [
+
+    # functions, math
+
+    'hello_world.py',                   # function structure
+    'hello_world2.py',                  # str type, concat
+    'numbers_division.py'               # int and float types
+    'personalized_hello_world.py',      # function arguments
+    'age_message_fix.py',                   # dynamically typed, can't concat str + int.
+    'get_seconds.py',                   # math operators
+    'get_century.py',                   # math operators
+    'bad_average.py',                   # parentheses in math
+    'calculator1.py',                   # built in functions type function call
+    'calculator2.py',                   # convert types calculator2 str + str
+    'tax_calc.py',                      # function with multiple arguments, variable scope
+    'bad_function.py',                  # function troubleshoot
+
+
+    # if simple
+
+    'fill_a_cab.py'                     # simple if 
+    'time_convertor.py'                 # if, ==, elif 
+    'is_even.py'                        # bool
+    'can_drive.py'                      # and 
+    'can_drive2.py'                     # or operators
+    'assess_temperature.py'             # if, elif, else
+
+]
